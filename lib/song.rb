de5c76e4c3dc69d8a09310a77f8553230c519c59
Ben Blackman <bblkmn5@gmail.com>
2017-10-13 17:03:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,25 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@count = 0
-
+@@genres = []
+@@artists = []
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
   @@count += 1
+  @@artists = artist ||= @@artists
+  @@genres = genres ||= @@genres
 end
 
-def count(count)
-  count
+def self.count
+  @@count
 end
+
+def self.genres_count
+  @@genres
+end
+
+
 
 end
