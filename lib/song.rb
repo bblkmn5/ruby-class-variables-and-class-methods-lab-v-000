7889ba99f96d10ab43dcb0762e58126ee3b4a86e
Ben Blackman <bblkmn5@gmail.com>
2017-10-13 17:29:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,14 @@ end
 
 def self.genre_count
   genre_count = {}
-
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
+  end
+  genre_count
 end
 
 def self.artists
